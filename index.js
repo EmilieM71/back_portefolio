5f4c30e0745063ada0da2e77a64925eb3d6daf91
Emilie <martelemilie@hotmail.fr>
2021-02-03 21:13:11 +0100

    create the server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+const api = require("./routes");
+
+app.use("/api", api);
+
+app.listen(5000, (err) => {
+  if (err) {
+    process.exit(1);
+  } else {
+    console.log(`Express server listening on ${port}`);
+  }
+});
